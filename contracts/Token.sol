c4cc71686c48d035a4f726e75d6967c22350a195
EwanDebove <ewan.debove@gmail.com>
2019-12-04 12:14:31 +0100

    contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,13 +1,24 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.4.21 <0.6.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
 
 contract MyNFT is ERC721Full, ERC721Mintable {
+  
+  string public name;
+  uint256  lastId;
+
   constructor() ERC721Full("MyNFT", "MNFT") public {
+  	lastId = 0;
   }
-  
+  function setname(string memory _name) public {
+  	name = _name;
+  }
 
+  function mint() public payable {
+  	require(msg.value == 100 );
+  	_safeMint(msg.sender, lastId);
+  	lastId = lastId +1;
+  }
 
-  
 }
